3c3f41196d4cfa3dc33071456376021d71bd72d9
Jan Stastka <jan.stastka12@gmail.com>
2024-01-13 15:42:47 +0100

    Added basic todo functionality

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,43 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import PageIcon from './components/icons/PageIcon.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <RouterLink to="/">
+      <PageIcon />
+    </RouterLink>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
   </header>
 
-  <RouterView />
+  <div>
+    <RouterView />
+  </div>
+
+  <footer>
+    <p>&copy;{{ new Date().getFullYear() }} Jan Šťástka | By Jan Šťástka</p>
+  </footer>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: .5rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header nav a {
+  margin: 0 .1em;
+  padding: .5em;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
+footer {
+  padding: .5em;
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,56 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+type taskListType = Array<{
+  id: number
+  name: string
+  completed: boolean
+}>
+
+const taskList = ref<taskListType>([{
+  id: 1,
+  name: 'Clean kitchen',
+  completed: false
+}])
+
+let id = ref(2)
+
+let newTaskName = ref('')
+
+const addTask = () => {
+  if (newTaskName.value.length < 1) return
+
+  taskList.value.push({
+    id: id.value,
+    name: newTaskName.value || '',
+    completed: false
+  })
+
+  id.value++
+  newTaskName.value = ''
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <form @submit.prevent="addTask">
+      <label for="newTask">Task name: </label>
+      <br />
+      <input type="text" name="newTask" id="newTask" v-model="newTaskName" placeholder="Clean kitchen">
+      <button>Add</button>
+    </form>
+
+    <ul>
+      <li v-for="{ id, name, completed } in taskList" :key="id" :class="{ complete: completed }">
+        {{ name }}
+      </li>
+    </ul>
   </main>
 </template>
+
+<style scoped>
+ul {
+  padding: 0;
+  list-style-type: none;
+}
+</style>
