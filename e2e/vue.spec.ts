09c50a728538074dd5873a496a00a83e7c51438b
Jan Stastka <jan.stastka12@gmail.com>
2024-02-09 11:00:06 +0100

    Added E2E test - complete all tasks

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -16,17 +16,16 @@ test.describe('Todo list functionality', () => {
   })
 
   test('Remove task', async ({ page }) => {
-    expect(await page.locator('ul > li').count()).toBe(1)
     await page.getByLabel('delete task').click()
     await page.waitForTimeout(550)
     expect(await page.locator('ul > li').count()).toBe(0)
   })
 
   test('Remove two tasks and remove the first one', async ({ page }) => {
-    expect(await page.locator('ul > li').count()).toBe(1)
     await page.locator('form > input').fill('New task 2')
     await page.locator('form > button').click()
     expect(await page.locator('ul > li').count()).toBe(2)
+
     await page.locator('ul > li:first-of-type div > button:last-of-type').click()
     await page.waitForTimeout(550)
     expect(await page.locator('ul > li').count()).toBe(1)
@@ -34,8 +33,16 @@ test.describe('Todo list functionality', () => {
   })
 
   test('Complete task', async ({ page }) => {
-    expect(await page.locator('ul > li').count()).toBe(1)
+    expect(await page.locator('ul > li').textContent()).not.toContain('(completed)')
     await page.locator('ul > li div > button:first-of-type').click()
     expect(await page.locator('ul > li').textContent()).toContain('(completed)')
+  })
+
+  test('Complete all tasks', async ({ page }) => {
+    await page.locator('form > input').fill('New task 2')
+    await page.locator('form > button').click()
+    await page.locator('div.buttons > button:last-of-type').click()
+    expect(await page.locator('ul > li:first-of-type').textContent()).toContain('(completed)')
+    expect(await page.locator('ul > li:last-of-type').textContent()).toContain('(completed)')
   })
 })
